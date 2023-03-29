3d062c080127c0837b9db97edb6ffb87760878d0
Jitender-Singh-NWM <jithu.cbit@gmail.com>
2023-03-29 07:54:30 -0500

    parseInt

--- routes/computation.js
+++ routes/computation.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var randdomval=Math.abs(Math.random() * (10 - 1) + 1);
+    var randdomval=parseInt(Math.random() * (10 - 1) + 1);
   res.send('[fn] applied to '+randdomval+' is '+Math.log(randdomval));
 });
 
