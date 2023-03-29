0c7410dcdeae269b4d3b82dcf100e5dd440888fa
Jitender-Singh-NWM <jithu.cbit@gmail.com>
2023-03-29 07:50:00 -0500

    Bonus update

--- routes/computation.js
+++ routes/computation.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var sum=0;
-var more=0;
+
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  more=more+1;
-  sum=sum+more;
-  res.send('Sum is:'+sum);
+    var randdomval=Math.random() * (10 - 1) + 1;
+  res.send('[fn] applied to '+randdomval+' is '+Math.log(randdomval));
 });
 
 module.exports = router;
